2b1ef51ad88fa4b61042e454c2441b4c1fbe1a74
collinsasia <106357786+collinsasia@users.noreply.github.com>
2022-06-03 19:43:42 -0400

    Update main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+const scoreBoard = document.querySelector(".status");
+const winnerAlert = () => "The winner is + ${CurrentPlayer} !"
+const drawAlert = () => "There is a DRAW!"
+const presentPlayerGo = () "${currentPlayer} + its your turn..."
+// applying constructor function
